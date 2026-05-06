5f8dde1723fab9c0365d94b2658435a6483806c2
Yulia Kozak <ykozak74@gmail.com>
2026-05-06 22:21:48 +0300

    task-6

---
+++ src/task-6.ts
@@ -0,0 +1,7 @@
+function getFirstElement(arr: T[]): T {
+  return arr[0];
+}
+
+getFirstElement([1, 2, 3]);           // 1
+getFirstElement(["a", "b", "c"]);     // "a"
+getFirstElement([true, false, true]); // true
